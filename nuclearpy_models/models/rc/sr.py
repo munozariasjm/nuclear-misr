a3c484875690d20bbc9d9a9c55507f5d46e0061f
munozariasjm <munozariasjm@gmail.com>
2024-03-13 21:11:12 -0400

    Uncertainty

--- nuclearpy_models/models/rc/sr.py
+++ nuclearpy_models/models/rc/sr.py
@@ -26,7 +26,7 @@ class SrRc:
     def __init__(self):
 
         self.expr_dict = {
-            0: "K*(0.9502984598011356 + 1.34*(1/Z)) + 0.017042713515038155*(P - (Z/N))*np.exp(S) - S",
+            0: "K*(0.9502984598011356 + 1.36*(1/Z)) + 0.017042713515038155*(P - (Z/N))*np.exp(S) - S",
             1: "(np.square(0.06607824565977677) * (d - (((((((np.square(P) - np.exp(0.8557356856877293)) / h) * np.exp(3.267293299752022)) + A) / np.square(Z)) * (np.exp(3.267293299752022) - np.square(P ** 1.0436541692574126))) + 1.6967299664570696)))",
             2: "(((on - 3.2638621674211596) + oz) * (-6.212472578529901e-5 * (oz - ((on - oz) / 0.8149545777451943))))",
             3: "((((h ** N) - P) + 1.6075150804926084) / -954.0159520953583)",
@@ -37,29 +37,60 @@ class SrRc:
             8: "((((-0.6164943592049766 + (((oz / h) * -0.019535405196981315) - -0.019535405196981315)) ** N) / 0.6109634910349819) * oo)",
             9: "((-0.0004231223865735906 - (((on ** oo) * (-0.0052107021784933705 * on)) / A)) / h)",
             10: "(((0.001489590152797498 / (1.162580718921562 - oz)) * (((oe * h) + eo) - P)) / 0.33113516554290906)",
-            11: "((((oz * ((2.5112647929755023 / (Z + -0.37892908364382)) * 1.0319078765296386)) ** Z) + S) * -0.0037334035169424708)",
-            12: "(((((0.7043599688790316 * -0.18018570969789297) + 0.12820228100582828) / (0.23845339742013413 - S)) / 1.0438595661715504) ** Z)",
-            13: "((0.0016509066361613859 / (on + -9.448405598196786)) * ((P ** 1.3702681472747735) / 1.525287162904831))",
+            # 11: "((((oz * ((2.5112647929755023 / (Z + -0.37892908364382)) * 1.0319078765296386)) ** Z) + S) * -0.0037334035169424708)",
+            # 12: "(((((0.7043599688790316 * -0.18018570969789297) + 0.12820228100582828) / (0.23845339742013413 - S)) / 1.0438595661715504) ** Z)",
+            # 13: "((0.0016509066361613859 / (on + -9.448405598196786)) * ((P ** 1.3702681472747735) / 1.525287162904831))",
         }
         self.max_index = len(list(self.expr_dict.keys())) - 1
+        self.optimal_params_mean = np.array(
+            [
+                1.00159966,
+                1.14549367,
+                1.13821116,
+                1.45251976,
+                1.21183011,
+                1.09285672,
+                0.95328495,
+                -0.08159929,
+                1.02284745,
+                1.55923727,
+            ]
+        )
+        self.optimal_params_std = np.array(
+            [
+                3.03947602e-04,
+                6.20367988e-02,
+                8.10207149e-02,
+                4.20530466e-01,
+                5.71420112e-01,
+                3.74672574e-01,
+                6.64226845e-02,
+                2.45251729e-01,
+                6.75270036e-01,
+                7.40305174e-01,
+            ]
+        )
 
-    def firs_order_bayesian(self, Z, N):
-        pass  # TODO
+    def get_expresion_term(self, index):
+        return self.expr_dict[index]
 
-    def __call__(self, Z, N, index=0) -> float:
+    def __call__(self, Z, N, index=-1, bst=True) -> float:
         features = self._get_features(Z, N)
         if index == -1:
-            index = len(self.expr_dict) - 1
-        if index == 0:
-            complete_model = self.expr_dict[0]
-        else:
-            complete_model = " + ".join([self.expr_dict[i] for i in range(0, index)])
-        symbonic_terms_pred = self.predict_symb_terms(features, complete_model)
-        return symbonic_terms_pred
+            index = self.max_index - 1
+        elif index == 0:
+            return self.predict_symb_terms(
+                features, self.get_expresion_term(0)
+            ), np.abs(self.predict_symb_terms(features, self.get_expresion_term(1)))
 
-    def predict_single_term(self, Z, N, index):
-        features = self._get_features(Z, N)
-        return self.predict_symb_terms(features, self.expr_dict[index])
+        terms = [self.get_expresion_term(i) for i in range(0, index)]
+        if not terms:
+            terms = [self.get_expresion_term(0)]
+        bst_output, unc = self.predict_with_uncertainty_boostrapping(Z, N, index)
+        if not bst:
+            return np.sum(self.predict_symb_terms(features, terms[index])), unc
+        else:
+            return bst_output, unc
 
     @staticmethod
     def _get_features(Z, N):
@@ -68,8 +99,6 @@ class SrRc:
         """
         z_magic_numbers = [2, 8, 20, 28, 50, 82, 126]
         n_magic_numbers = [2, 8, 20, 28, 50, 82, 126, 184]
-        # νp(n) is the difference between the proton (neutron) number
-        # of a particular nucleus and the nearest magic number.
         clossest_z = min(z_magic_numbers, key=lambda x: abs(x - Z))
         clossest_n = min(n_magic_numbers, key=lambda x: abs(x - N))
         vp = abs(Z - clossest_z)
@@ -110,6 +139,31 @@ class SrRc:
     @staticmethod
     def predict_symb_terms(features, model_str):
         return eval(model_str, None, features)
+
+    def predict_term(self, Z, N, term):
+        feats = self._get_features(Z, N)
+        return self.predict_symb_terms(feats, term)
+
+    def predict_with_uncertainty_boostrapping(
+        self,
+        Z,
+        N,
+        index,
+        N_ITERATIONS=10_000,
+    ):
+        terms = [self.get_expresion_term(i) for i in range(0, index)]
+        preds_per_term = np.array([self.predict_term(Z, N, term) for term in terms])
+        preds = np.dot(
+            np.random.normal(
+                self.optimal_params_mean[: len(terms)],
+                self.optimal_params_std[: len(terms)],
+                (N_ITERATIONS, len(terms)),
+            ),
+            preds_per_term,
+        )
+        extra_term = self.predict_term(Z, N, self.get_expresion_term(index + 1))
+        unc = np.max(np.abs(preds - preds.mean(axis=0)), axis=0) + np.abs(extra_term)
+        return preds.mean(), unc
 
 
 sr_rc = SrRc()
